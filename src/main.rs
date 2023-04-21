4c7a2c4c5116bcd556841ca2818c5aafdfe3ccf9
ChristianMascolo <mascolokris@gmail.com>
2023-04-21 15:17:25 +0200

    new push

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use ::bevy::{
 
 use bevy::{
     prelude::{
-        AssetServer, Assets, Camera2dBundle, Commands, Mesh, Res, ResMut, Transform, Vec2, Query, OrthographicProjection, With, Camera,
+        AssetServer, Assets, Bundle, Camera2d, Camera2dBundle, Commands, Component,
+        GlobalTransform, Mesh, OrthographicProjection, Query, Res, ResMut, Transform, Vec3,
     },
-    sprite::{ColorMaterial, MaterialMesh2dBundle, Sprite, SpriteBundle}, time::Time,
+    sprite::{ColorMaterial, MaterialMesh2dBundle, Sprite, SpriteBundle},
 };
 
 use geo_bevy::{build_bevy_meshes, BuildBevyMeshesContext};
@@ -27,29 +28,7 @@ fn main() {
         }))
         .add_startup_system(setup)
         //.add_system(zoom_camera)
-        //.add_system(setup)
         .run();
-}
-
-fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
-    // commands.spawn(SpriteBundle {
-    //     sprite: Sprite {
-    //         custom_size: Some(Vec2 { x: 50., y: 50. }),
-    //         ..Default::default()
-    //     },
-    //     ..Default::default()
-    // });
-}
-
-fn zoom_camera(mut query: Query<&mut OrthographicProjection, With<Camera>>, time: Res<Time>){
-    for mut projection in query.iter_mut() {
-        let mut log_scale = projection.scale.ln();
-        log_scale -= 0.1 * time.delta_seconds();
-        projection.scale = log_scale.exp();
-
-        println!("Current zoom scale: {}", projection.scale);
-    }
 }
 
 fn setup(
@@ -59,10 +38,8 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let feature_collection =
-        read_geojson_feature_collection(read_geojson("maps/only_polygon.geojson".to_owned()));
-    let mut i: f32 = 0.;
-
-    commands.spawn(Camera2dBundle::default());
+        read_geojson_feature_collection(read_geojson("maps/point_and_line.geojson".to_owned()));
+    let mut i = 0;
 
     for feature in feature_collection {
         let geometry = feature.geometry.unwrap();
@@ -71,19 +48,16 @@ fn setup(
             .unwrap()
             .collect::<Vec<_>>();
 
-        // println!("geom: {:?}", geom);
         for prepared_mesh in mesh_iter {
             match prepared_mesh {
                 geo_bevy::PreparedMesh::Point(points) => {
                     for geo::Point(coord) in points.iter() {
                         let color = Color::RED;
-                        let mut offset = Vec2::new(coord.x as f32, coord.y as f32);
-                        offset *= 2.0; // Modifica la costante dell'offset come preferisci
-                        let mut transform =
-                            Transform::from_xyz(coord.x as f32, coord.y as f32, i * i);
-                        transform.translation += offset.extend(0.0); // Aggiunge l'offset alla posizione di traduzione
-
-                        println!("{:?}", transform.translation);
+                        let transform = Transform::from_xyz(
+                            coord.x as f32 + (i as f32) * (i as f32),
+                            coord.y as f32 + (i as f32) * (i as f32),
+                            (i as f32) * (i as f32),
+                        );
 
                         let bundle = SpriteBundle {
                             sprite: Sprite {
@@ -94,20 +68,22 @@ fn setup(
                             transform,
                             ..Default::default()
                         };
+
                         commands.spawn(bundle);
 
-                        i += 1.;
+                        i += 1;
                     }
                 }
                 geo_bevy::PreparedMesh::Polygon { mesh, color } => {
                     println!("Inside Polygon, topology:{:?}", mesh.primitive_topology());
-                    println!("Mesh indices: {:?}",mesh.indices());
+                    println!("Mesh indices: {:?}", mesh.indices());
+
                     let material = materials.add(color.into());
+
                     commands.spawn(MaterialMesh2dBundle {
                         material,
                         mesh: bevy::sprite::Mesh2dHandle(assets_meshes.add(mesh)),
-                        transform: Transform::from_xyz(0., 0., 0.),
-                        visibility: bevy::render::view::Visibility { is_visible: true },
+                        transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
                         ..Default::default()
                     });
                 }
@@ -116,17 +92,32 @@ fn setup(
                         "Inside LineString, topology: {:?}",
                         mesh.primitive_topology()
                     );
+
                     let material = materials.add(color.into());
 
                     commands.spawn(MaterialMesh2dBundle {
                         material,
                         mesh: bevy::sprite::Mesh2dHandle(assets_meshes.add(mesh)),
-                        transform: Transform::from_xyz(0., 0., 1.),
+                        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 1.0)),
                         visibility: bevy::render::view::Visibility { is_visible: true },
                         ..Default::default()
                     });
                 }
             }
         }
+    }
+
+    commands.spawn(Camera2dBundle {
+        camera_2d: bevy::prelude::Camera2d {
+            clear_color: bevy::core_pipeline::clear_color::ClearColorConfig::None,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+}
+
+fn zoom_camera(mut query: Query<(&Camera2d, &mut OrthographicProjection)>) {
+    for (_, mut projection) in query.iter_mut() {
+        projection.scale = 2.0;
     }
 }
