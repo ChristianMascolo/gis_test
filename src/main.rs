5ec5c22851f8dcc486f5b8fcfbb98de7af74725d
ChristianMascolo <mascolokris@gmail.com>
2023-12-14 16:17:10 +0100

    lyon

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,19 @@ use ::bevy::{
 
 use bevy::{
     core_pipeline::core_2d::Camera2dBundle,
-    math::Vec3,
+    math::{Vec3, Vec2},
     prelude::{AssetServer, Assets, ClearColor, Commands, Mesh, Res, ResMut},
     sprite::{ColorMaterial, MaterialMesh2dBundle, Sprite, SpriteBundle},
     transform::components::Transform,
-    window::Windows,
+    window::{Windows, WindowMode}, render::camera::Camera, input::mouse::MouseButton, ecs::system::Query,
 };
-
+use bevy_prototype_lyon::{prelude::*, entity::ShapeBundle};
 use bevy_inspector_egui::bevy_egui::EguiPlugin;
 use bevy_inspector_egui::bevy_inspector;
 
-use geo::Centroid;
+use geo::{Centroid, CoordsIter};
 use geo_bevy::{build_bevy_meshes, BuildBevyMeshesContext};
-use geo_types::Point;
+use geo_types::{Point, Geometry, Coord};
 use gis_layers::AllLayers;
 use gis_test::{read_geojson, read_geojson_feature_collection};
 
@@ -58,6 +58,7 @@ fn main() {
     app.add_plugin(bevy::sprite::SpritePlugin::default());
     app.add_plugin(EguiPlugin);
     app.add_plugin(bevy_inspector_egui::DefaultInspectorConfigPlugin);
+    app.add_plugin(ShapePlugin);
 
     // systems
     app.add_startup_system(setup);
@@ -76,89 +77,162 @@ fn setup(
 ) {
     let mut layers = gis_layers::AllLayers::new();
     let feature_collection =
-        read_geojson_feature_collection(read_geojson("maps/all_geometry.geojson".to_owned()));
+        read_geojson_feature_collection(read_geojson("C:/Users/masco/gis_test-1/maps/all_geometry.geojson".to_owned()));
     let mut i = 0;
     let primary_window = windows.get_primary().unwrap();
 
     for feature in feature_collection {
         let geometry = feature.geometry.unwrap();
         let geom: geo_types::geometry::Geometry<f64> = geometry.try_into().unwrap();
+        //let mesh_iter = build_bevy_meshes(&geom, Color::RED, BuildBevyMeshesContext::new())
+        //    .unwrap()
+        //    .collect::<Vec<_>>();
 
-        let mesh_iter = build_bevy_meshes(&geom, Color::RED, BuildBevyMeshesContext::new())
-            .unwrap()
-            .collect::<Vec<_>>();
-
-        let _ = layers.add(geom, "mesh".to_owned());
+        let _ = layers.add(geom.clone(), "mesh".to_owned());
         
-        for prepared_mesh in mesh_iter {
-            match prepared_mesh {
-                geo_bevy::PreparedMesh::Point(points) => {
-                    for geo::Point(coord) in points.iter() {
-                        let color = Color::RED;
-                        let last_id = layers.last_layer_id();
-                        let z_index = calculate_z(last_id, MeshType::Point) + i;
-                        let transform = bevy::prelude::Transform::from_xyz(
-                            coord.x as f32,
-                            coord.y as f32,
-                            z_index as f32,
-                        );
+        match geom{
+            Geometry::Polygon(_) => {
+                let mut coords = Vec::new();
+    
+                for coord in geom.coords_iter(){ coords.push((coord.x as f32,coord.y as f32).into()) }
+                
+                let shape = bevy_prototype_lyon::shapes::Polygon{
+                    points: coords,
+                    closed: true,
+                };
 
-                        let bundle = SpriteBundle {
-                            sprite: Sprite {
-                                color,
-                                ..Default::default()
-                            },
-                            texture: asset_server.load("circle.png"),
-                            transform,
-                            ..Default::default()
-                        };
+                let translation = Vec3 { x: 0. , y: 0., z: calculate_z(layers.last_layer_id(), MeshType::Polygon) as f32 };
 
-                        commands.spawn(bundle);
-                        i = i + 1;
-                    }
-                }
+                commands.spawn(
+                        GeometryBuilder::build_as(&shape,DrawMode::Fill(FillMode::color(Color::CYAN)),Transform::from_translation(translation)),
+                );
 
-                geo_bevy::PreparedMesh::Polygon { mesh, color } => {
-                    let last_id = layers.last_layer_id();
-                    let material = materials.add(color.into());
-                    let z_index = calculate_z(last_id, MeshType::Polygon);
-                    let transform = bevy::prelude::Transform::from_translation(Vec3::new(
-                        0.,
-                        0.,
-                        z_index as f32,
-                    ));
+            },
+            Geometry::LineString(_) => {
+                let mut coords = Vec::new();
+    
+                for coord in geom.coords_iter(){ coords.push((coord.x as f32,coord.y as f32).into()) }
+                
+                let shape = bevy_prototype_lyon::shapes::Polygon{
+                    points: coords,
+                    closed: false,
+                };
 
-                    commands
-                        .spawn(MaterialMesh2dBundle {
-                            material,
-                            mesh: bevy::sprite::Mesh2dHandle(assets_meshes.add(mesh.clone())),
-                            transform: transform,
-                            visibility: bevy::render::view::Visibility { is_visible: true },
-                            ..Default::default()
-                        });
-                }
+                let translation = Vec3 { x: 0. , y: 0., z: calculate_z(layers.last_layer_id(), MeshType::Polygon) as f32 };
 
-                geo_bevy::PreparedMesh::LineString { mesh, color } => {
-                    let last_id = layers.last_layer_id();
-                    let material = materials.add(color.into());
-                    let z_index = calculate_z(last_id, MeshType::LineString);
-                    let transform = bevy::prelude::Transform::from_translation(Vec3::new(
-                        0.,
-                        0.,
-                        z_index as f32,
-                    ));
+                commands.spawn(
+                        GeometryBuilder::build_as(&shape,DrawMode::Fill(FillMode::color(Color::CYAN)),Transform::from_translation(translation)),
+                );
+            },
+            Geometry::Point(_) =>{
+                let shape = shapes::Circle{
+                    radius: 1.,
+                    center: Vec2::new(geom.centroid().unwrap().x()as f32 ,geom.centroid().unwrap().y() as f32),
+                };
 
-                    commands
-                        .spawn(MaterialMesh2dBundle {
-                            material,
-                            mesh: bevy::sprite::Mesh2dHandle(assets_meshes.add(mesh.clone())),
-                            transform: transform,
-                            visibility: bevy::render::view::Visibility { is_visible: true },
-                            ..Default::default()
-                        });
-                }
-            }
+                let translation = Vec3 { x: 0. , y: 0., z: calculate_z(layers.last_layer_id(), MeshType::Point) as f32 };
+
+                commands.spawn(
+                        GeometryBuilder::build_as(&shape,DrawMode::Fill(FillMode::color(Color::CYAN)),Transform::from_translation(translation)),
+                );
+
+            },
+            Geometry::Line(_) => {
+                let mut coords = Vec::new();
+    
+                for coord in geom.coords_iter(){ coords.push((coord.x as f32,coord.y as f32).into()) }
+                
+                let shape = bevy_prototype_lyon::shapes::Polygon{
+                    points: coords,
+                    closed: false,
+                };
+
+                let translation = Vec3 { x: 0. , y: 0., z: calculate_z(layers.last_layer_id(), MeshType::Polygon) as f32 };
+
+                commands.spawn(
+                        GeometryBuilder::build_as(&shape,DrawMode::Fill(FillMode::color(Color::CYAN)),Transform::from_translation(translation)),
+                );
+            },
+            Geometry::MultiPoint(_) => todo!(),
+            Geometry::MultiLineString(_) => todo!(),
+            Geometry::MultiPolygon(_) => todo!(),
+            Geometry::GeometryCollection(_) => todo!(),
+            Geometry::Rect(_) => todo!(),
+            Geometry::Triangle(_) => todo!(),
         }
+
+        //for prepared_mesh in mesh_iter {
+        //    match prepared_mesh {
+        //        geo_bevy::PreparedMesh::Point(points) => {
+        //            for geo::Point(coord) in points.iter() {
+        //                let color = Color::RED;
+        //                let last_id = layers.last_layer_id();
+        //                let z_index = calculate_z(last_id, MeshType::Point) + i;
+        //                let mut transform = bevy::prelude::Transform::from_xyz(
+        //                    coord.x as f32,
+        //                    coord.y as f32,
+        //                    z_index as f32,
+        //                );
+//
+        //                let bundle = SpriteBundle {
+        //                    sprite: Sprite {
+        //                        color,
+        //                        custom_size: Some(bevy::math::Vec2::new(1.,1.)),
+        //                        ..Default::default()
+        //                    },
+        //                    texture: asset_server.load("circle.png"),
+        //                    transform,
+        //                    ..Default::default()
+        //                };
+//
+        //                commands.spawn(bundle);
+        //                i = i + 1;
+        //            }
+        //        }
+//
+        //        //geo_bevy::PreparedMesh::Polygon { mesh, color } => {
+        //        //    let last_id = layers.last_layer_id();
+        //        //    let material = materials.add(color.into());
+        //        //    let z_index = calculate_z(last_id, MeshType::Polygon);
+        //        //    let transform = bevy::prelude::Transform::from_translation(Vec3::new(
+        //        //        0.,
+        //        //        0.,
+        //        //        z_index as f32,
+        //        //    ));
+////
+////
+        //        //    commands
+        //        //        .spawn(MaterialMesh2dBundle {
+        //        //            material,
+        //        //            mesh: bevy::sprite::Mesh2dHandle(assets_meshes.add(mesh.clone())),
+        //        //            transform: transform,
+        //        //            visibility: bevy::render::view::Visibility { is_visible: true },
+        //        //            ..Default::default()
+        //        //        });
+        //        //}
+//
+        //        geo_bevy::PreparedMesh::LineString { mesh, color } => {
+        //            let last_id = layers.last_layer_id();
+        //            let material = materials.add(color.into());
+        //            let z_index = calculate_z(last_id, MeshType::LineString);
+        //            let transform = bevy::prelude::Transform::from_translation(Vec3::new(
+        //                0.,
+        //                0.,
+        //                z_index as f32,
+        //            ));
+//
+        //            commands
+        //                .spawn(MaterialMesh2dBundle {
+        //                    material,
+        //                    mesh: bevy::sprite::Mesh2dHandle(assets_meshes.add(mesh.clone())),
+        //                    transform: transform,
+        //                    visibility: bevy::render::view::Visibility { is_visible: true },
+        //                    ..Default::default()
+        //                });
+        //        }
+        //        geo_bevy::PreparedMesh::Polygon { mesh, color } => todo!(),
+        //    }
+        //}
 
         }
 
@@ -209,16 +283,11 @@ fn get_all_centroids(layers: AllLayers) -> Vec<Point>{
 
 fn create_camera(centroids: Vec<Point>, win_width: f32, win_height: f32) -> Camera2dBundle{
     let center = medium_centroid(centroids);
-    //let min_width = (win_width - center.0.x as  f32) / 2.;
-    //let min_height = (win_height - center.0.y as  f32) / 2.;
 
     Camera2dBundle {
         projection: bevy::render::camera::OrthographicProjection {
-            scaling_mode: bevy::render::camera::ScalingMode::Auto {
-                min_width:  win_width / 2.,
-                min_height:  win_height / 2.
-            },
-            scale: 0.001,
+            scaling_mode: bevy::render::camera::ScalingMode::WindowSize,
+            scale: 0.1,
             ..Default::default()
         },
         transform: Transform::from_xyz(center.0.x as f32, center.0.y as f32, 999.9),
