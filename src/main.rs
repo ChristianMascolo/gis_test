9f649300b97bfeba4778fc038ae7eabebd72303d
ChristianMascolo <mascolokris@gmail.com>
2024-02-01 12:24:34 +0100

    implementazione selezione file

--- src/main.rs
+++ src/main.rs
@@ -2,71 +2,26 @@ mod gis_layer_id;
 mod gis_layers;
 
 use ::bevy::{
-    prelude::{App, Color},
+    prelude::*,
     window::{WindowDescriptor, WindowPlugin},
 };
 
-use bevy::prelude::*;
+use bevy_egui::{
+    egui::{self, Color32, RichText},
+    EguiContext, EguiPlugin,
+};
 
-use bevy_inspector_egui::bevy_egui::EguiPlugin;
-use bevy_inspector_egui::bevy_inspector;
 use bevy_pancam::PanCam;
 use bevy_prototype_lyon::prelude::*;
-use eframe::{
-    egui::{CentralPanel, Context},
-    App as OtherApp, Frame,
-};
-use egui_file::FileDialog;
 use geo::Centroid;
 use geo_types::{Geometry, Point};
 use gis_test::*;
-use std::{
-    ffi::OsStr,
-    path::{Path, PathBuf},
-};
-
-#[derive(Default)]
-pub struct Demo {
-    opened_file: Option<PathBuf>,
-    open_file_dialog: Option<FileDialog>,
-}
-
-impl OtherApp for Demo {
-    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
-        CentralPanel::default().show(ctx, |ui| {
-            if (ui.button("Open")).clicked() {
-                // Show only files with the extension "txt".
-                let filter = Box::new({
-                    let ext = Some(OsStr::new("txt"));
-                    move |path: &Path| -> bool { path.extension() == ext }
-                });
-                let mut dialog =
-                    FileDialog::open_file(self.opened_file.clone()).show_new_folder(true);
-                dialog.open();
-                self.open_file_dialog = Some(dialog);
-            }
-
-            if let Some(dialog) = &mut self.open_file_dialog {
-                if dialog.show(ctx).selected() {
-                    if let Some(file) = dialog.path() {
-                        self.opened_file = Some(file.to_path_buf());
-                    }
-                }
-            }
-        });
-    }
-}
 
 fn main() {
     let mut app = App::new();
-    //let _ = eframe::run_native(
-    //    "File Dialog Demo",
-    //    eframe::NativeOptions::default(),
-    //    Box::new(|_cc| Box::new(Demo::default())),
-    //);
 
     // resources
-    app.insert_resource(ClearColor(Color::rgb(255., 255., 255.)));
+    app.insert_resource(ClearColor(Color::BLACK));
 
     // plugins
     app.add_plugins(bevy::MinimalPlugins);
@@ -86,25 +41,48 @@ fn main() {
     app.add_plugin(bevy::core_pipeline::CorePipelinePlugin::default());
     app.add_plugin(bevy::transform::TransformPlugin::default());
     app.add_plugin(bevy::sprite::SpritePlugin::default());
-    app.add_plugin(bevy_inspector_egui::DefaultInspectorConfigPlugin);
     app.add_plugin(bevy_pancam::PanCamPlugin::default());
-    app.add_plugin(EguiPlugin);
     app.add_plugin(ShapePlugin);
+    app.add_plugin(EguiPlugin);
 
     // systems
     app.add_startup_system(setup);
-    app.add_system(inspector_ui);
 
     // run
     app.run();
 }
 
 fn setup(
-    mut commands: Commands,
+    mut egui_context: ResMut<EguiContext>,
+    commands: Commands,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<ColorMaterial>>,
+) {
+    
+    egui::SidePanel::left("main").show(egui_context.ctx_mut(), |ui| {
+        ui.vertical_centered(|ui| {
+            ui.heading("GIS");
+            ui.separator();
+            let file_button =
+                egui::Button::new(RichText::new("▶ Select File").color(Color32::GREEN));
+
+            if ui.add(file_button).clicked() {
+                if let Some(path) = rfd::FileDialog::new().pick_file(){
+                    let file_path = Some(path.display().to_string()).unwrap();
+                    build_meshes(meshes, materials, commands, file_path);
+                }
+            }
+        })
+    });
+}
+
+fn build_meshes(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    let geojson = read_geojson("C:/Users/masco/gis_test-1/maps/campania.geojson".to_owned());
+    mut commands: Commands,
+    file_path: String
+){
+    let geojson = read_geojson(file_path);
     let feature_collection = read_geojson_feature_collection(geojson);
     let mut layers: gis_layers::AllLayers = gis_layers::AllLayers::new();
 
@@ -215,29 +193,7 @@ fn setup(
 
     commands
         .spawn(setup_camera(layers))
-        .insert(PanCam::default());
-}
-
-fn inspector_ui(world: &mut bevy::ecs::world::World) {
-    let egui_context = world
-        .resource_mut::<bevy_inspector_egui::bevy_egui::EguiContext>()
-        .ctx_mut()
-        .clone();
-
-    bevy_inspector_egui::egui::Window::new("UI").show(&egui_context, |ui| {
-        bevy_inspector_egui::egui::ScrollArea::vertical().show(ui, |ui| {
-            // equivalent to `WorldInspectorPlugin`
-            bevy_inspector::ui_for_world(world, ui);
-
-            // works with any `Reflect` value, including `Handle`s
-            let mut any_reflect_value: i32 = 5;
-            bevy_inspector::ui_for_value(&mut any_reflect_value, ui, world);
-
-            bevy_inspector_egui::egui::CollapsingHeader::new("Materials").show(ui, |ui| {
-                bevy_inspector::ui_for_assets::<bevy::pbr::StandardMaterial>(world, ui);
-            });
-        });
-    });
+        .insert(PanCam::default());    
 }
 
 fn setup_camera(layers: gis_layers::AllLayers) -> Camera2dBundle {
