8f518d0193aaaef9d065dde03d83008c168a3713
ChristianMascolo <mascolokris@gmail.com>
2024-02-03 16:46:09 +0100

    implementazione pulsante remove per i singoli file

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,18 @@ use bevy_egui::{
 };
 
 use bevy_pancam::PanCam;
-use bevy_prototype_lyon::{entity, prelude::*};
+use bevy_prototype_lyon::prelude::*;
 use geo::Centroid;
-use geo_types::{Geometry, Point};
+use geo_types::Geometry;
 use gis_layers::AllLayers;
 use gis_test::*;
 
 #[derive(Component, Clone)]
 struct Files {
     file_name: String,
+    file_path: String,
     file_layers: AllLayers,
+    file_entities: Vec<Entity>,
 }
 
 fn main() {
@@ -78,7 +80,7 @@ fn ui(
     let camera = camera_query.get_single().unwrap();
     egui::SidePanel::left("main").show(egui_context.ctx_mut(), |ui| {
         ui.vertical_centered(|ui| {
-            ui.heading("GIS");
+            ui.heading(RichText::new("GIS").color(Color32::RED).strong());
             ui.separator();
             let select_btn =
                 egui::Button::new(RichText::new("▶ Select File").color(Color32::GREEN));
@@ -89,16 +91,18 @@ fn ui(
                 if let Some(path) = rfd::FileDialog::new().pick_file() {
                     let file_path = Some(path.display().to_string()).unwrap();
                     let file_name = path.file_name().unwrap().to_str().unwrap();
-                    let layers = build_meshes(
+                    let (layers,entities) = build_meshes(
                         meshes,
                         materials,
                         &mut commands,
-                        file_path,
+                        file_path.to_owned(),
                         file_name.to_owned(),
                     );
                     let files = Files {
                         file_name: file_name.to_owned(),
+                        file_path: file_path.to_owned(),
                         file_layers: layers,
+                        file_entities: entities
                     };
                     let mut vec_files: Vec<Files> = Vec::new();
 
@@ -123,6 +127,26 @@ fn ui(
             if ui.add(exit_btn).clicked() {
                 app_exit_events.send(bevy::app::AppExit);
             }
+
+            ui.separator();
+
+            for file in files_query.iter(){
+                let file_name = &file.file_name;
+                let add_file_btn = egui::Button::new(RichText::new("Add").color(Color32::WHITE));
+                let remove_file_btn = egui::Button::new(RichText::new("Remove").color(Color32::WHITE));
+                let label_text = file_name.to_owned() + " actions";
+               
+                ui.label(RichText::new(label_text).strong());
+                
+                if ui.add(add_file_btn).clicked(){
+                }
+
+                if ui.add(remove_file_btn).clicked(){
+                    despawn_entities_file(&mut commands, file.clone());
+                }
+                
+                ui.separator();
+            }
         })
     });
 }
@@ -133,10 +157,11 @@ fn build_meshes(
     commands: &mut Commands,
     file_path: String,
     file_name: String,
-) -> AllLayers {
+) -> (AllLayers,Vec<Entity>) {
     let geojson = read_geojson(file_path);
     let feature_collection = read_geojson_feature_collection(geojson);
     let mut layers: gis_layers::AllLayers = gis_layers::AllLayers::new();
+    let mut entities_id: Vec<Entity> = Vec::new();
 
     for feature in feature_collection {
         let geometry = feature.geometry.unwrap();
@@ -148,13 +173,15 @@ fn build_meshes(
 
                 let (builder, transform) = build_polygon(polygon, layers.last_layer_id());
 
-                commands.spawn(builder.build(
+                let id = commands.spawn(builder.build(
                     DrawMode::Outlined {
                         fill_mode: FillMode::color(Color::WHITE),
                         outline_mode: StrokeMode::new(Color::BLUE, 0.1),
                     },
                     transform,
-                ));
+                )).id();
+
+                entities_id.push(id);
             }
             Geometry::LineString(linestring) => {
                 layers.add(
@@ -164,17 +191,19 @@ fn build_meshes(
 
                 let (builder, transform) = build_linestring(linestring, layers.last_layer_id());
 
-                commands.spawn(builder.build(
+                let id = commands.spawn(builder.build(
                     DrawMode::Stroke(StrokeMode::color(Color::YELLOW_GREEN)),
                     transform,
-                ));
+                )).id();
+
+                entities_id.push(id);
             }
             Geometry::Point(point) => {
                 let center = point.centroid();
                 layers.add(geom.clone(), file_name.clone());
                 let z = calculate_z(layers.last_layer_id(), MeshType::Point);
 
-                commands.spawn(bevy::sprite::MaterialMesh2dBundle {
+                let id = commands.spawn(bevy::sprite::MaterialMesh2dBundle {
                     mesh: meshes.add(shape::Circle::new(1.).into()).into(),
                     material: materials.add(Color::PINK.into()),
                     transform: Transform::from_translation(Vec3::new(
@@ -183,7 +212,9 @@ fn build_meshes(
                         z,
                     )),
                     ..Default::default()
-                });
+                }).id();
+
+                entities_id.push(id);
             }
             Geometry::MultiPolygon(multi_polygon) => {
                 layers.add(
@@ -195,13 +226,15 @@ fn build_meshes(
                     let (builder, transform) =
                         build_polygon(polygon.clone(), layers.last_layer_id());
 
-                    commands.spawn(builder.build(
+                    let id = commands.spawn(builder.build(
                         DrawMode::Outlined {
                             fill_mode: FillMode::color(Color::WHITE),
                             outline_mode: StrokeMode::new(Color::BLUE, 0.1),
                         },
                         transform,
-                    ));
+                    )).id();
+
+                    entities_id.push(id);
                 }
             }
             Geometry::MultiLineString(multi_line_string) => {
@@ -214,17 +247,19 @@ fn build_meshes(
                     let (builder, transform) =
                         build_linestring(line.clone(), layers.last_layer_id());
 
-                    commands.spawn(builder.build(
+                    let id = commands.spawn(builder.build(
                         DrawMode::Stroke(StrokeMode::color(Color::YELLOW_GREEN)),
                         transform,
-                    ));
+                    )).id();
+
+                    entities_id.push(id);
                 }
             }
             Geometry::MultiPoint(multi_point) => {
                 for point in multi_point {
                     layers.add(geo::Geometry::Point(point), file_name.clone());
                     let z = calculate_z(layers.last_layer_id(), MeshType::Point);
-                    commands.spawn(bevy::sprite::MaterialMesh2dBundle {
+                    let id = commands.spawn(bevy::sprite::MaterialMesh2dBundle {
                         mesh: meshes.add(shape::Circle::new(1.).into()).into(),
                         material: materials.add(Color::PINK.into()),
                         transform: Transform::from_translation(Vec3::new(
@@ -233,14 +268,16 @@ fn build_meshes(
                             z,
                         )),
                         ..Default::default()
-                    });
+                    }).id();
+
+                    entities_id.push(id);
                 }
             }
             _ => continue,
         }
     }
 
-    layers
+    (layers,entities_id)
 }
 
 fn center_camera(commands: &mut Commands, camera: Entity, files: Vec<Files>) {
@@ -264,4 +301,10 @@ fn center_camera(commands: &mut Commands, camera: Entity, files: Vec<Files>) {
     new_camera.transform = Transform::from_xyz(center.0.x as f32, center.0.y as f32, 999.9);
 
     commands.spawn(new_camera).insert(PanCam::default());
+}
+
+fn despawn_entities_file(commands: &mut Commands, file: Files){
+    for entity in file.file_entities.iter(){
+        commands.entity(*entity).despawn();
+    }
 }
